0079a94de5376b6373131ee394443607ffd6e61b
bianca Hoang <biancahoang@biancas-MacBook-Pro.local>
2023-02-17 11:03:40 -0500

    🎭 initial structure

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const basicAuth = require('express-basic-auth');
+const httpsRedirect = require('express-https-redirect');
+const publicPath = path.join(__dirname, 'build');
+const PORT = process.env.PORT || 4000;
+
+if (process.env.REACT_APP_AUTH_OVERLAY == "true"){
+
+  app.use(basicAuth({
+    users: { 
+      'admin': 'yorlie',
+      'yorlie': process.env.SERVER_PASSSWORD
+    },
+    challenge: true,
+  }))
+}
+app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(publicPath));
+app.use('/', httpsRedirect());
+app.get('*', (req, res) => {
+   res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+app.listen(PORT, () => console.log(`Listening on :${PORT}`))
